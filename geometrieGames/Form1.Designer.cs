db3a6b20e50bf8ee420f57b48b0e7f7571b7ab3a
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:26:03 +0200

    Add project files.

---
+++ geometrieGames/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace geometrieGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new graphicswithpermanentcontrol.UserControl1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.White;
+            this.userControl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userControl11.Location = new System.Drawing.Point(25, 2);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(268, 249);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.MouseMove += new System.Windows.Forms.MouseEventHandler(this.userControl11_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(17, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 273);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private graphicswithpermanentcontrol.UserControl1 userControl11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ geometrieGames/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace geometrieGames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int x = 0;
+        public int y = 0;
+        public int x1 = 10;
+        public int y1 = 10;
+        public int x2 = 40;
+        public int y2 = 40;
+
+        public bool eInInterior()
+        {
+            if ((x > x1  && x < x2 + 10) && (y > y1 && y < y2 + 10))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void userControl11_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            Text = eInInterior().ToString() + " : "  + e.X.ToString() + " : " + e.Y.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControl11.g.DrawRectangle(userControl11.p, x1, y1, x2, y2);
+            userControl11.Refresh();
+        }
+    }
+}
